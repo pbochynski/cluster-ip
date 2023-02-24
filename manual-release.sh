ca09e7dcafb48a22a63c90842a4206b29b09fba3
pbochynski <p.bochynski@gmail.com>
2023-02-24 10:34:45 +0100

    Configure tags (sha)

--- manual-release.sh
+++ manual-release.sh
@@ -11,6 +11,10 @@ kyma alpha create module -n kyma-project.io/cluster-ip --version $IMAGE_TAG \
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.10+k3s1" K3S_KUBECONFIG_MODE=600 INSTALL_K3S_EXEC="server --disable traefik" sh -
 
+git clone git@github.com:pbochynski/cluster-ip.git
+cd cluster-ip
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 export IMAGE_TAG=$(git rev-parse --short HEAD) 
-make IMG=ghcr.io/pbochynski/cluster-ip:$IMAGE_TAG
+make install
+make docker-build IMG=ghcr.io/pbochynski/cluster-ip:$IMAGE_TAG
+make deploy IMG=ghcr.io/pbochynski/cluster-ip:$IMAGE_TAG
